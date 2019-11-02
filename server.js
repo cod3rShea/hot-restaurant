8aa4b971db2a66c277d30e8af8202fa8b681145f
Sean Michael Shea <seanshea1337@gmail.com>
2019-11-02 13:43:07 -0400

    add user

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT =  process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -35,33 +35,63 @@ var waitList = [
 ];
 
 // index
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // table
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // reservations 
-app.get("/reservations", function(req, res) {
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 
 // Displays all tables
-app.get("/api/tables", function(req, res) {
+app.get("/api/tables", function (req, res) {
     return res.json(reservations);
 });
 
 // Displays all waiting list
-app.get("/api/waitlist", function(req, res) {
+app.get("/api/waitlist", function (req, res) {
     return res.json(waitList);
 });
-  
 
-app.listen(PORT, function() {
+var reservationsLength = reservations.length;
+
+if (reservationsLength < 6) {
+    function test() {
+        event.preventDefault();
+        alert('test');
+        var newCharacter = {
+            customerName: jQuery("#person-name").val().trim(),
+            customerEmail: jQuery("#phone-number").val().trim(),
+            customerID: jQuery("#user-email").val().trim(),
+            phoneNumber: jQuery("#customer-id").val().trim()
+        };
+        jQuery.post("/api/tables", newCharacter).then(function (data) {
+                alert("Added reservation");
+        });
+
+    }
+} else {
+    function test() {
+        event.preventDefault();
+        var newCharacter = {
+            customerName: jQuery("#person-name").val().trim(),
+            customerEmail: jQuery("#phone-number").val().trim(),
+            customerID: jQuery("#user-email").val().trim(),
+            phoneNumber: jQuery("#customer-id").val().trim()
+        };
+        jQuery.post("/api/waitlist", newCharacter).then(function (data) {
+                alert("Added to wait list");
+        });
+    }
+}
+
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
-  
