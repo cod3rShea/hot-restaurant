698aa59e5eee73ec9cd9376d7dce3beaf1372239
Sean Michael Shea <seanshea1337@gmail.com>
2019-11-02 12:45:49 -0400

    add herokuapp port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT =  process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -28,15 +28,17 @@ var waitList = [
 
 ];
 
-
+// index
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// table
 app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
+// reservations 
 app.get("/reservations", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
